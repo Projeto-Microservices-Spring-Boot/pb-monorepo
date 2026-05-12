937eab419c6f6fe7c46861799319adb27764334f
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2026-05-12 17:02:26 -0300

    feat(profile): cria service  já dockerizado e conectado ao eureka

---
+++ services/profile/src/test/java/com/edu/infnet/pb/profile/ProfileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.infnet.pb.profile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
