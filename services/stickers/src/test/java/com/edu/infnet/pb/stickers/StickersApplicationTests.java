9544f64229407d7874eb819a66fc5e7b6be347de
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2026-05-12 17:02:02 -0300

    feat(stickers): cria service já dockerizado e conectado ao eureka

---
+++ services/stickers/src/test/java/com/edu/infnet/pb/stickers/StickersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.infnet.pb.stickers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StickersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
