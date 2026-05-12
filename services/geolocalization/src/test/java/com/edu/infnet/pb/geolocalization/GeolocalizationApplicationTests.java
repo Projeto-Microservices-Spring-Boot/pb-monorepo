b6f23e6ba2aa8e447d0c8f7f41dd3893a6f0eea6
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2026-05-12 17:03:08 -0300

    feat(geoloc): cria service já dockerizado e conectado ao eureka

---
+++ services/geolocalization/src/test/java/com/edu/infnet/pb/geolocalization/GeolocalizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.infnet.pb.geolocalization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeolocalizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
