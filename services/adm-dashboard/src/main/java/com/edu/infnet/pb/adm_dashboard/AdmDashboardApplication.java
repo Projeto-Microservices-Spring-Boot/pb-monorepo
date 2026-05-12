6e7ceca529019ee6296cf0f3d5b4d2a09bd9b9b6
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2026-05-12 17:07:07 -0300

    feat(adm dash): cria service store já dockerizado e conectado ao eureka

---
+++ services/adm-dashboard/src/main/java/com/edu/infnet/pb/adm_dashboard/AdmDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.edu.infnet.pb.adm_dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdmDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdmDashboardApplication.class, args);
+	}
+
+}
---
+++ services/adm-dashboard/src/test/java/com/edu/infnet/pb/adm_dashboard/AdmDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.infnet.pb.adm_dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdmDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
