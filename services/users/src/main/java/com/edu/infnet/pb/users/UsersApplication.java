aa7e8d332c4290eb5794375d9bf8ab7d368af4d9
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2026-05-17 22:08:55 -0300

    refactor: remove microservices de auth e profile e os unifica no microservice de users

---
+++ services/users/src/main/java/com/edu/infnet/pb/users/UsersApplication.java
@@ -0,0 +1,13 @@
+package com.edu.infnet.pb.users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersApplication.class, args);
+	}
+
+}
