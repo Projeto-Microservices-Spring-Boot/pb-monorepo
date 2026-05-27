5ef21515a4ad5cc2b8ed4d2304dc1db5bc175ce2
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2026-05-27 18:27:59 -0300

    feat(users): cria model inicial de usuários

---
+++ services/users/src/main/java/com/edu/infnet/pb/users/domain/models/User.java
@@ -0,0 +1,40 @@
+package com.edu.infnet.pb.users.domain.models;
+
+import java.util.UUID;
+
+import com.edu.infnet.pb.users.domain.enums.Roles;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class User {
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private UUID id;
+
+  private String name;
+
+  @Column(unique = true)
+  private String email;
+
+  private String password;
+
+  @Enumerated(EnumType.STRING)
+  private Roles roles;
+}
