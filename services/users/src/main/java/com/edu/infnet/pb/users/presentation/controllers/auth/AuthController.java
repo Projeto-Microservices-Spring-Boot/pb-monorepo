fbc20b8793b8cbc7fc1b19c545bd3d33078b5e30
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2026-05-29 16:29:18 -0300

    feat(auth): cria endpoints iniciais de register e login

--- services/users/src/main/java/com/edu/infnet/pb/users/presentation/controllers/auth/AuthController.java
+++ services/users/src/main/java/com/edu/infnet/pb/users/presentation/controllers/auth/AuthController.java
@@ -1,25 +1,35 @@
 package com.edu.infnet.pb.users.presentation.controllers.auth;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.edu.infnet.pb.users.application.services.auth.AuthService;
+import com.edu.infnet.pb.users.presentation.dtos.auth.LoginRequestDto;
+import com.edu.infnet.pb.users.presentation.dtos.auth.LoginResponseDto;
+import com.edu.infnet.pb.users.presentation.dtos.auth.RegisterRequestDto;
+import com.edu.infnet.pb.users.presentation.dtos.auth.RegisterResponseDto;
 
 import lombok.RequiredArgsConstructor;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-
-import com.edu.infnet.pb.users.presentation.dtos.auth.LoginRequestDto;
-import com.edu.infnet.pb.users.presentation.dtos.auth.LoginResponseDto;
-
 @RestController
+@RequestMapping("auth")
 @RequiredArgsConstructor
 public class AuthController {
-  private final JwtEncoder jwtEncoder;
+  private final AuthService service;
+
+  @PostMapping("/register")
+  public ResponseEntity<RegisterResponseDto> register(@RequestBody RegisterRequestDto body) {
+    var result = service.register(body);
+    return ResponseEntity.ok(result);
+  }
 
   @PostMapping("/login")
-  public ResponseEntity<LoginResponseDto> login(@RequestBody LoginRequestDto loginRequest) {
-    
+  public ResponseEntity<LoginResponseDto> login(@RequestBody LoginRequestDto body) {
+    var result = service.login(body);
+    return ResponseEntity.ok(result);
   }
 
 }
