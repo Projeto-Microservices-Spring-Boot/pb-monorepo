8efb9c2a0769d085defd74919ead3d8b4389c3ff
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2026-05-29 16:24:53 -0300

    feat(repo): adiciona métodos complementares ao repositório de auth
    Métodos para buscar por e-mail, nome e nome da role

--- services/users/src/main/java/com/edu/infnet/pb/users/infrastructure/repositories/AuthRepository.java
+++ services/users/src/main/java/com/edu/infnet/pb/users/infrastructure/repositories/AuthRepository.java
@@ -1,13 +1,19 @@
 package com.edu.infnet.pb.users.infrastructure.repositories;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.edu.infnet.pb.users.domain.enums.Roles;
 import com.edu.infnet.pb.users.domain.models.User;
 
 @Repository
 public interface AuthRepository extends JpaRepository<User, UUID> {
+  Optional<User> findByEmail(String email);
 
+  Optional<User> findByName(String name);
+
+  Optional<User> findRoleByName(Roles role);
 }
