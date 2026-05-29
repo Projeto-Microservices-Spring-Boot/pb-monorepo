208ef202969ec371621866bb1e3fd260b56a6624
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2026-05-29 16:22:53 -0300

    feat(dtos): cria DTOs para request e response de cadastro

---
+++ services/users/src/main/java/com/edu/infnet/pb/users/presentation/dtos/auth/RegisterResponseDto.java
@@ -0,0 +1,7 @@
+package com.edu.infnet.pb.users.presentation.dtos.auth;
+
+import com.edu.infnet.pb.users.domain.enums.Roles;
+
+public record RegisterResponseDto(String name, String email, Roles role, String refreshToken) {
+
+}
