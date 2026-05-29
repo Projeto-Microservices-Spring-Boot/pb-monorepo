fe0b0a1babcc11ec12a394298ff5c92bc215a52d
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2026-05-29 16:23:51 -0300

    feat(dto): adiciona  refreshToken no DTO de resposta de login

--- services/users/src/main/java/com/edu/infnet/pb/users/presentation/dtos/auth/LoginResponseDto.java
+++ services/users/src/main/java/com/edu/infnet/pb/users/presentation/dtos/auth/LoginResponseDto.java
@@ -1,5 +1,5 @@
 package com.edu.infnet.pb.users.presentation.dtos.auth;
 
-public record LoginResponseDto(String accessToken, Long expiresIn) {
+public record LoginResponseDto(String accessToken, String refreshToken, Long expiresIn) {
 
 }
