56dca6f5a36bf48ce0ee487d6e15e6fb9bd59372
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2026-06-01 19:32:43 -0300

    refactor(auth): Remove refresh token do DTO de resposta de registro

--- services/users/src/main/java/com/edu/infnet/pb/users/presentation/dtos/auth/RegisterResponseDto.java
+++ services/users/src/main/java/com/edu/infnet/pb/users/presentation/dtos/auth/RegisterResponseDto.java
@@ -2,6 +2,6 @@ package com.edu.infnet.pb.users.presentation.dtos.auth;
 
 import com.edu.infnet.pb.users.domain.enums.Roles;
 
-public record RegisterResponseDto(String name, String email, Roles role, String refreshToken) {
+public record RegisterResponseDto(String name, String email, Roles role) {
 
 }
