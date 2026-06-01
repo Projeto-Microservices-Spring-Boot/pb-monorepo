5deccd4b98822d2c333c68f1f8dfa8859ec809e0
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2026-06-01 18:01:49 -0300

    feat(profile): adiciona logs de erro e usa exceção customizada para usuário não encontrado

--- services/users/src/main/java/com/edu/infnet/pb/users/application/services/profile/ProfileService.java
+++ services/users/src/main/java/com/edu/infnet/pb/users/application/services/profile/ProfileService.java
@@ -6,20 +6,26 @@ import org.springframework.stereotype.Service;
 
 import com.edu.infnet.pb.users.infrastructure.repositories.ProfileRepository;
 import com.edu.infnet.pb.users.presentation.dtos.auth.ProfileResponseDto;
+import com.edu.infnet.pb.users.shared.exceptions.ResourceNotFoundException;
 
-import jakarta.ws.rs.NotFoundException;
 import lombok.RequiredArgsConstructor;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @Service
 @RequiredArgsConstructor
 public class ProfileService {
   private final ProfileRepository repo;
 
+  private static final Logger logger = LogManager.getLogger(ProfileService.class);
+
   public ProfileResponseDto getProfile(UUID userId) {
     var userExists = repo.findById(userId);
 
     if (userExists.isEmpty()) {
-      throw new NotFoundException("user not found");
+      logger.error("Usuário não encontrado!");
+      throw new ResourceNotFoundException("Usuário não encontrado!");
     }
 
     return new ProfileResponseDto(userExists.get().getName());
