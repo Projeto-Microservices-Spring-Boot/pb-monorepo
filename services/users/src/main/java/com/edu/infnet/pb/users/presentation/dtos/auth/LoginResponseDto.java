6102e17b9c7d43eebe6ca83514eed41451081118
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2026-06-05 14:22:24 -0300

    feat(auth): adiciona tempo de expiracao do refresh token e renomeia o de access

--- services/users/src/main/java/com/edu/infnet/pb/users/presentation/dtos/auth/LoginResponseDto.java
+++ services/users/src/main/java/com/edu/infnet/pb/users/presentation/dtos/auth/LoginResponseDto.java
@@ -1,5 +1,6 @@
 package com.edu.infnet.pb.users.presentation.dtos.auth;
 
-public record LoginResponseDto(String accessToken, String refreshToken, Long expiresIn) {
+public record LoginResponseDto(String accessToken, String refreshToken, Long refreshTokenExpiresIn,
+    Long accessTokenExpiresIn) {
 
 }
