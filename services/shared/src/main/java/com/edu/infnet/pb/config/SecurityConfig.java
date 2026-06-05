7d91208f7bd6223dc4da021e39aae1784c091f5e
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2026-06-04 22:03:29 -0300

    feat(security): move e simplifica SecurityConfig para shared
    Esse arquivo será usado por todos os services para ter acesso as funções do jwt

--- services/shared/src/main/java/com/edu/infnet/pb/config/SecurityConfig.java
+++ services/shared/src/main/java/com/edu/infnet/pb/config/SecurityConfig.java
@@ -1,6 +1,5 @@
-package com.edu.infnet.pb.users.config;
+package com.edu.infnet.pb.config;
 
-import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
 
 import org.springframework.beans.factory.annotation.Value;
@@ -12,15 +11,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
-import com.nimbusds.jose.jwk.JWK;
-import com.nimbusds.jose.jwk.JWKSet;
-import com.nimbusds.jose.jwk.RSAKey;
-import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
 
 @Configuration
 @EnableWebSecurity
@@ -28,30 +20,20 @@ public class SecurityConfig {
   @Value("${jwt.public.key}")
   private RSAPublicKey rsaPublicKey;
 
-  @Value("${jwt.private.key}")
-  private RSAPrivateKey rsaPrivateKey;
-
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
     http
         .authorizeHttpRequests(auth -> auth
-            .requestMatchers("/actuator/**").permitAll() /* <- Rotas públicas */
+            .requestMatchers("/actuator/**").permitAll()
             .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
             .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
-            .anyRequest().authenticated()) /* <- Rotas privadas */
+            .anyRequest().authenticated())
         .csrf(csrf -> csrf.disable())
-        .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults())) /* Ativa validacao JWT nas requests */
+        .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
         .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
     return http.build();
-  }
-
-  @Bean
-  public JwtEncoder jwtEncoder() {
-    JWK jwk = new RSAKey.Builder(this.rsaPublicKey).privateKey(rsaPrivateKey).build();
-    var jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
-    return new NimbusJwtEncoder(jwks);
   }
 
   @Bean
