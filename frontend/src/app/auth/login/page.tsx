bd77cb6b5ed63f338f338c3010d65a5532929356
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2026-06-09 20:19:25 -0300

    feat(auth): adiciona tela de login

---
+++ frontend/src/app/auth/login/page.tsx
@@ -0,0 +1,10 @@
+import { Card } from '@/presentation/components/ui/card';
+import { LoginForm } from '@/presentation/components/auth/login-form';
+
+export default function LoginPage() {
+  return (
+    <Card>
+      <LoginForm />
+    </Card>
+  );
+}
