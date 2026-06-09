2f83879603a45626991047204b6a42d00c37de09
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2026-06-09 20:19:41 -0300

    feat(auth): adiciona tela de cadastro

---
+++ frontend/src/app/auth/register/page.tsx
@@ -0,0 +1,10 @@
+import { Card } from '@/presentation/components/ui/card';
+import { RegisterForm } from '@/presentation/components/auth/register-form';
+
+export default function RegisterPage() {
+  return (
+    <Card>
+      <RegisterForm />
+    </Card>
+  );
+}
