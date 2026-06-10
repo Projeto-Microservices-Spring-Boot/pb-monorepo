24a7f472f5863719d07d6ca6f481bd9a18ad220d
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2026-06-09 21:33:25 -0300

    docs(auth): documenta o controller de auth com Swagger

--- services/users/src/main/java/com/edu/infnet/pb/users/presentation/controllers/auth/AuthController.java
+++ services/users/src/main/java/com/edu/infnet/pb/users/presentation/controllers/auth/AuthController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.edu.infnet.pb.users.application.services.auth.AuthService;
+import com.edu.infnet.pb.users.docs.AuthControllerDocs;
 import com.edu.infnet.pb.users.presentation.dtos.auth.LoginRequestDto;
 import com.edu.infnet.pb.users.presentation.dtos.auth.LoginResponseDto;
 import com.edu.infnet.pb.users.presentation.dtos.auth.RegisterRequestDto;
@@ -21,7 +22,7 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping("auth")
 @RequiredArgsConstructor
-public class AuthController {
+public class AuthController implements AuthControllerDocs {
   private final AuthService service;
 
   @PostMapping("/register")
@@ -37,16 +38,18 @@ public class AuthController {
   }
 
   @PostMapping("/logout")
-  public void logout(@AuthenticationPrincipal Jwt jwt) {
+  public ResponseEntity<Void> logout(@AuthenticationPrincipal Jwt jwt) {
     UUID userId = UUID.fromString(jwt.getSubject());
     service.Logout(userId);
+    return ResponseEntity.noContent().build();
   }
 
   // @PostMapping("/refresh")
-  // public ResponseEntity<LoginResponseDto> refresh(@AuthenticationPrincipal Jwt jwt,
-  //     @RequestParam() String refreshToken) {
-  //   UUID userId = UUID.fromString(jwt.getSubject());
-  //   var result = service.refresh(refreshToken, userId);
-  //   return ResponseEntity.ok(result);
+  // public ResponseEntity<LoginResponseDto> refresh(@AuthenticationPrincipal Jwt
+  // jwt,
+  // @RequestParam() String refreshToken) {
+  // UUID userId = UUID.fromString(jwt.getSubject());
+  // var result = service.refresh(refreshToken, userId);
+  // return ResponseEntity.ok(result);
   // }
 }
