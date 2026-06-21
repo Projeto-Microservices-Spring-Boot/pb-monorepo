7bff67a256d1b34bc1b77b161c74c345a484477d
DarkDzi <eduardoassisaraujo@gmail.com>
2026-06-21 17:33:48 -0300

    feat(kafka): adicionar dto para comunicação

---
+++ services/stickers/src/main/java/com/edu/infnet/pb/stickers/Communication/CollectionTransferConsumer.java
@@ -0,0 +1,28 @@
+package com.edu.infnet.pb.stickers.Communication;
+
+import com.edu.infnet.pb.stickers.Dto.Communication.CollectionTranferReponse;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CollectionTransferConsumer {
+    private static final Logger log = LogManager.getLogger(CollectionTransferConsumer.class);
+
+    @Retryable(
+            retryFor = Exception.class,  // retry para qualquer exceção
+            maxAttempts = 3,             // tenta 3 vezes
+            backoff = @Backoff(delay = 2000) // espera 2s entre tentativas
+    )
+
+    @KafkaListener(topics = "Collection-Transfer-response", groupId = "stickers-service")
+    public void receive(CollectionTranferReponse response) {
+        log.info("Mensagem recebida do geo");
+
+
+
+    }
+}
---
+++ services/stickers/src/main/java/com/edu/infnet/pb/stickers/Dto/Communication/CollectionTranferReponse.java
@@ -0,0 +1,5 @@
+package com.edu.infnet.pb.stickers.Dto.Communication;
+
+public class CollectionTranferReponse {
+
+}
