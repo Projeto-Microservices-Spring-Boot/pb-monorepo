ecec80ba4e77ccab1861549653b59afa07a5ff5f
DarkDzi <eduardoassisaraujo@gmail.com>
2026-06-22 17:06:01 -0300

    feat(kafka): comunicação kafka

--- services/stickers/src/main/java/com/edu/infnet/pb/stickers/Communication/CollectionTransferConsumer.java
+++ services/stickers/src/main/java/com/edu/infnet/pb/stickers/Communication/CollectionTransferConsumer.java
@@ -1,6 +1,9 @@
 package com.edu.infnet.pb.stickers.Communication;
 
-import com.edu.infnet.pb.stickers.Dto.Communication.CollectionTranferReponse;
+
+import com.edu.infnet.pb.stickers.Dto.Communication.PropostaAceitaEvent;
+import com.edu.infnet.pb.stickers.Service.CollectionTransferService;
+import lombok.RequiredArgsConstructor;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -8,20 +11,20 @@ import org.springframework.stereotype.Service;
 import org.springframework.kafka.support.Acknowledgment;
 
 @Service
+@RequiredArgsConstructor
 public class CollectionTransferConsumer {
     private static final Logger log = LogManager.getLogger(CollectionTransferConsumer.class);
+    private final CollectionTransferService transferService;
 
     @KafkaListener(topics = "collection-transfer", groupId = "stickers-service", containerFactory = "kafkaListenerContainerFactory")
-    public void receive(CollectionTranferReponse response, Acknowledgment ack) {
-        log.info("Mensagem recebida do geo");
+    public void receive(PropostaAceitaEvent event, Acknowledgment ack) {
+        log.info("Mensagem recebida do geo: propostaId={} usuarioOrigem={} usuarioDestino={}",
+                event.getPropostaId(), event.getUsuarioOrigem(), event.getUsuarioDestino());
         try{
-
-
-
+            transferService.applyTransfer(event);
             ack.acknowledge(); // commita e muda offset
         }catch(Exception e){
             log.error("Falha ao processar mensagem",e);
-
             throw e;
         }
 
--- services/stickers/src/main/java/com/edu/infnet/pb/stickers/Dto/Communication/CollectionTranferReponse.java
+++
@@ -1,5 +0,0 @@
-package com.edu.infnet.pb.stickers.Dto.Communication;
-
-public class CollectionTranferReponse {
-
-}
