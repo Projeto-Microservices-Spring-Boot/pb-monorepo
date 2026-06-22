0d71cfde8403665aae24c88ba9d6de14a0f399a8
DarkDzi <eduardoassisaraujo@gmail.com>
2026-06-22 14:10:44 -0300

    feat(kafka): adiciona ack(commit manual) ao consumer

--- services/stickers/src/main/java/com/edu/infnet/pb/stickers/Communication/CollectionTransferConsumer.java
+++ services/stickers/src/main/java/com/edu/infnet/pb/stickers/Communication/CollectionTransferConsumer.java
@@ -5,17 +5,20 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
+import org.springframework.kafka.support.Acknowledgment;
 
 @Service
 public class CollectionTransferConsumer {
     private static final Logger log = LogManager.getLogger(CollectionTransferConsumer.class);
 
     @KafkaListener(topics = "collection-transfer", groupId = "stickers-service", containerFactory = "kafkaListenerContainerFactory")
-    public void receive(CollectionTranferReponse response) {
+    public void receive(CollectionTranferReponse response, Acknowledgment ack) {
         log.info("Mensagem recebida do geo");
         try{
 
 
+
+            ack.acknowledge(); // commita e muda offset
         }catch(Exception e){
             log.error("Falha ao processar mensagem",e);
 
