e763d7d6fcf5aee5e21f9390d4aed6b52c5ec629
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2026-06-22 22:27:13 -0300

    fix(auth): adiciona novamente os endpoints de swagger

--- services/shared/src/main/java/com/edu/infnet/pb/config/SecurityConfig.java
+++ services/shared/src/main/java/com/edu/infnet/pb/config/SecurityConfig.java
@@ -35,6 +35,7 @@ public class SecurityConfig {
     http
         .authorizeHttpRequests(auth -> auth
             .requestMatchers("/actuator/**").permitAll()
+            .requestMatchers("/v3/api-docs/**", "/swagger-ui/**").permitAll()
             .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
             .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
             .anyRequest().authenticated())
