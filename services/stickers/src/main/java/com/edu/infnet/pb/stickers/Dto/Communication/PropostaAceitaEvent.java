87fbbbf008162b88fa089849855853c3a84dff25
DarkDzi <eduardoassisaraujo@gmail.com>
2026-06-22 22:22:27 -0300

    feat(communication): adiciona dtos atualizados

--- services/stickers/src/main/java/com/edu/infnet/pb/stickers/Dto/Communication/PropostaAceitaEvent.java
+++ services/stickers/src/main/java/com/edu/infnet/pb/stickers/Dto/Communication/PropostaAceitaEvent.java
@@ -3,6 +3,8 @@ package com.edu.infnet.pb.stickers.Dto.Communication;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 @Getter
@@ -11,10 +13,8 @@ public class PropostaAceitaEvent {
     private Long propostaId;
     private UUID usuarioOrigem;
     private UUID usuarioDestino;
-    private Long stickerOferecidoOrigem;
-    private Integer quantidadeOferecidaOrigem;
-    private Long stickerOferecidoDestino;
-    private Integer quantidadeOferecidaDestino;
+    private List<StickerOfertDTO> stickerOferecidoOrigem;
+    private List<StickerOfertDTO> stickerOferecidoDestino;
+    private LocalDateTime dataAceite;
 
 }
-
