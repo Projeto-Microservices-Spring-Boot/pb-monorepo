0b84477fb5f00586eb5e93049bee79eaa0f0aaa9
DarkDzi <eduardoassisaraujo@gmail.com>
2026-06-25 02:26:41 -0300

    feat(resiliencia): ativa circtuibreaker no feign

--- services/stickers/src/main/java/com/edu/infnet/pb/stickers/Communication/TradePointConsumer.java
+++ services/stickers/src/main/java/com/edu/infnet/pb/stickers/Communication/TradePointConsumer.java
@@ -1,12 +1,13 @@
 package com.edu.infnet.pb.stickers.Communication;
 
 import com.edu.infnet.pb.stickers.Dto.Communication.Trocas;
+import org.springframework.cloud.openfeign.FallbackFactory;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
-@FeignClient(name="geolocalization") // Name do Service no eureka
+@FeignClient(name="geolocalization", fallbackFactory = TradePointConsumerFallbackFactory.class) // Name do Service no eureka
 public interface TradePointConsumer {
     @GetMapping("/maps/trocas")
     List<Trocas> getTrocas();
---
+++ services/stickers/src/main/java/com/edu/infnet/pb/stickers/Communication/TradePointConsumerFallbackFactory.java
@@ -0,0 +1,29 @@
+package com.edu.infnet.pb.stickers.Communication;
+
+import com.edu.infnet.pb.stickers.Dto.Communication.Trocas;
+import com.edu.infnet.pb.stickers.Exception.BusinessRuleException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.cloud.openfeign.FallbackFactory;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class TradePointConsumerFallbackFactory implements FallbackFactory<TradePointConsumer> {
+    private static final Logger log = LogManager.getLogger(TradePointConsumerFallbackFactory.class);
+
+    @Override
+    public TradePointConsumer create(Throwable cause) {
+        return new TradePointConsumer() {
+            @Override
+            public List<Trocas> getTrocas() {
+                log.error("Erro ao buscar pontos de troca: {}", cause.getMessage());
+                // Mock de pontos de trocas
+                throw new BusinessRuleException(
+                        "Serviço de pontos de troca temporariamente indisponível");
+            }
+        };
+    }
+
+}
