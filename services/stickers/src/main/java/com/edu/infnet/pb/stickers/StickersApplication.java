482b41e71dcd7d08930e586a9ed2b466abdb40b9
DarkDzi <eduardoassisaraujo@gmail.com>
2026-06-25 01:46:08 -0300

    feat(Feign): habilitado feign

--- services/stickers/src/main/java/com/edu/infnet/pb/stickers/StickersApplication.java
+++ services/stickers/src/main/java/com/edu/infnet/pb/stickers/StickersApplication.java
@@ -2,9 +2,10 @@ package com.edu.infnet.pb.stickers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.kafka.annotation.EnableKafka;
 
-
+@EnableFeignClients
 @SpringBootApplication(scanBasePackages = "com.edu.infnet.pb")
 public class StickersApplication {
 
